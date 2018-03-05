eddf50261c8153bdb9eaff5cdc03b3e3bf8b3d8b
jpboliv <jpoliveira11@gmail.com>
2018-03-05 15:17:20 +0000

    Fixed create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,8 @@ class PostsController < ApplicationController
     end
 
     def create
-        if(@post = Post.create(post_params))
+        @post = Post.create(post_params)
+        if(@post.valid?)
             flash[:success] = "Your post has been created!"
             redirect_to posts_path
         else
