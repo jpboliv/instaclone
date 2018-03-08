a7e7794c48643f39e84ccd4f2a0fb47d92eaa5ad
jpboliv <jpoliveira11@gmail.com>
2018-03-08 17:59:32 +0000

    fixed post deletion and load more button when there were no posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
     validates :caption, presence: true, length: { minimum: 3, maximum: 300 }
     has_attached_file :image, styles: { :medium => "640x" }
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-    belongs_to :user, dependent: :destroy
+    belongs_to :user
     has_many :comments, dependent: :destroy
   end
