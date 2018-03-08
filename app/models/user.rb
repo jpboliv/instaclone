8c918518c2d78283ae39c78ec7c9abd3f425fccc
jpboliv <jpoliveira11@gmail.com>
2018-03-08 12:51:41 +0000

    changed syling of likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :user_name, presence: true, length: {minimum: 4, maximum: 16}
-  
+  acts_as_voter
   has_many :posts
 end
