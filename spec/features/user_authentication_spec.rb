32ab0ff5a9c4fc0484e554212ff8fc5e3f96dcfc
jpboliv <jpoliveira11@gmail.com>
2018-03-09 12:50:51 +0000

    added further tests to user authentication. cant see posts without login, nor create a new post

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -16,5 +16,29 @@ feature 'User authentication' do
         fill_in 'password', with: 'illbeback'
         click_button 'Log in'
         expect(page).to have_content("Signed in successfully.")
+        expect(page).to have_content("New Post")
+        expect(page).to have_content("Logout")
+        expect(page).to_not have_content('Register')
+
+    end
+
+    scenario 'can log out once logged in' do
+        visit '/' 
+        click_link 'Login'
+        fill_in 'username', with: 'Arnie'
+        fill_in 'password', with: 'illbeback'
+        click_button 'Log in'
+        click_link 'Logout'
+        expect(page).to have_content('You need to sign in or sign up before continuing.')
+    end
+
+    scenario 'cannot view index posts whithout loggin in' do 
+        visit '/'
+        expect(page).to have_content("You need to sign in or sign up before continuing.")
+    end
+
+    scenario 'cannot create a new post without loggin in' do
+        visit '/posts/new'
+        expect(page).to have_content("You need to sign in or sign up before continuing.")
     end
 end
