f11c612147e027da6a38bccf2ceb315c1203169d
jpboliv <jpoliveira11@gmail.com>
2018-03-21 10:34:26 +0000

    Removed sleep on tests

--- spec/features/searching_user_spec.rb
+++ spec/features/searching_user_spec.rb
@@ -1,17 +1,15 @@
 require "rails_helper.rb"
 
-feature "Searching a User " do
-  scenario "Search through username", :js=>true do
+feature "Searching a User ", js: true do
+  scenario "Search through username" do
     user = create(:user)
     sign_in_with(user)
     fill_in("Search for a user", with: "Arnie")
-    sleep(3)
   end
 
-  scenario "Search through email",:js=>true do
+  scenario "Search through email" do
     user = create(:user)
     sign_in_with(user)
     fill_in("Search for a user", with: "fancyfrank@gmail.com")
-    sleep(3)
   end
 end
