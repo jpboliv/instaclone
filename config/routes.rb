4631413d85b72678d1b51f4f69e64761b002336e
jpboliv <jpoliveira11@gmail.com>
2018-03-21 14:58:50 +0000

    Removed extra spaces

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   get 'notifications', to: 'notifications#index'
   get 'profiles/show'
   devise_for :users
-  
+
   get 'posts', to: 'posts#index'
   get 'show', to: 'posts#index'
 
-  get 'search',to: 'posts#search'
+  get 'search', to: 'posts#search'
   root 'posts#index'
 
   get 'user/:user_name', to: 'profiles#show', as: :profile
